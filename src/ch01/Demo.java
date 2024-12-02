a10ee4e6649dea5c8cfb9a5702b8597fa3830f0b
sunsh <1283128767@qq.com>
2024-12-02 21:23:35 +0800

    241202

--- src/ch01/Demo.java
+++ src/ch01/Demo.java
@@ -1,10 +1,15 @@
 package src.ch01;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println(StringUtils.substring("Hello World!", 0, 6));
+        int data = 279;
+        int d1 = data / 100; // 2
+        System.out.println(d1);
+        int d2 = data % (100 / 10); // 7
+        System.out.println(d2);
+        int d3 = data % 10; // 9
+        System.out.println(d3);
+        int s = d1 + d2 + d3;
+        System.out.println("" + s);
     }
 }
